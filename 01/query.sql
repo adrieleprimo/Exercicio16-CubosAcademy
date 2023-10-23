9566de779a8045552c5b7ceea105e052b615b098
Adriele Primo <adriele.dejprimo@gmail.com>
2023-10-23 19:20:19 -0300

    Atividade sovre consultas sql realizada

--- 01/query.sql
+++ 01/query.sql
@@ -1,1 +1,18 @@
-
+SELECT * FROM musicas;
+SELECT compositor, id FROM musicas WHERE tempo > 60*4;
+SELECT compositor, tempo FROM musicas WHERE tempo > 60*4;
+SELECT compositor, composicao FROM musicas WHERE id BETWEEN 47 and 123;
+SELECT * FROM musicas WHERE compositor IS NOT NULL AND tempo > 60*5 AND compositor <> 'Bach';
+SELECT * FROM musicas ORDER BY id DESC;
+SELECT * FROM musicas ORDER BY tempo DESC;
+SELECT * FROM musicas ORDER BY tempo ASC LIMIT 5;
+SELECT * FROM musicas ORDER BY tempo DESC LIMIT 10;
+SELECT * FROM musicas ORDER BY tempo ASC LIMIT 10 OFFSET 5;
+SELECT * FROM musicas LIMIT 10 OFFSET 40;
+SELECT * FROM musicas LIMIT 10 OFFSET 60;
+SELECT DISTINCT compositor FROM musicas WHERE compositor IS NOT NULL  ;
+SELECT DISTINCT compositor, composicao FROM musicas;
+SELECT * FROM musicas WHERE compositor LIKE 'Bra%';
+SELECT * FROM musicas WHERE ritmo ILIKE '%troppo';
+SELECT * FROM musicas where composicao ILIKE '%quartet%';
+SELECT * FROM musicas WHERE composicao NOT ILIKE  '%quintet%';
